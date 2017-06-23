e5dcd5169cdad35aa39b0caafc8b5c27283215dd
anarcoqr <anarcoqr@gmail.com>
2017-06-23 16:31:24 -0300

    Falhando a questao do aviao

--- Haskell/alturaAviao.hs
+++ Haskell/alturaAviao.hs
@@ -1,17 +1,20 @@
 main = do
     x <- getLine
     let alturaIdeal = read x :: Int
-    estabilizaAviao(alturaIdeal)
+    estabilizaAviao(alturaIdeal, 0)
     
 
-estabilizaAviao :: Int -> (IO)
-estabilizaAviao (alturaIdeal)
+estabilizaAviao :: (Int, Int) -> IO()
+estabilizaAviao (alturaIdeal, limite)
     = do
       y <- getLine
       let altura = read y :: Int
-      if(limiteAtual(alturaIdeal, altura) == 0) then putStrLn("OK")
-      else if(limiteAtual(alturaIdeal, altura) >= 0) then putStrLn("PERIGO") ++ estabilizaAviao(alturaIdeal)
-      else putStrLn("ADEQUADO") ++ estabilizaAviao(alturaIdeal)
+      
+      if limiteAtual(alturaIdeal, altura) == 0 then putStrLn("OK")
+      else if limiteAtual(alturaIdeal, altura) > limite then putStrLn("PERIGO" ++ estabilizaAviao(alturaIdeal, limite))
+      else putStrLn("ADEQUADO" ++ estabilizaAviao(alturaIdeal, limiteAtual(alturaIdeal, altura)))
 
 limiteAtual :: (Int, Int) -> Int
-limiteAtual (x, y) = x - y 
+limiteAtual (x, y)
+    | x >= y = x - y
+    | otherwise = y - x
